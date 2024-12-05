4cd9921adddc97761985f5192fbb0aeb1f0f067d
strozz1 <oscar.gonzalez.strozzi14@gmail.com>
2024-12-05 18:27:31 +0100

    edit

--- nvim/.config/nvim/lua/oscar/remap.lua
+++ nvim/.config/nvim/lua/oscar/remap.lua
@@ -1,21 +1,29 @@
 local builtin = require('telescope.builtin')
 
+local function exec()
+    local buf = vim.api.nvim_create_buf(false, "nofile")
+    local window = vim.api.nvim_open_win(buf, false, {
+        split = "right",
+        win = 0,
+        width = 50
+    })
+    vim.api.nvim_win_set_option(window, 'wrap', true)
+    vim.api.nvim_win_set_option(window, 'relativenumber', false)
+    vim.api.nvim_win_set_option(window, 'number', false)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Compiling" })
+end
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Search files/words
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers,{})
-vim.keymap.set('n', '<C-s>', builtin.live_grep,{})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-s>', builtin.live_grep, {})
 
 
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-
-
-
-
-
+vim.keymap.set('n', '<leader>ef', exec, {})
 vim.keymap.set("x", "<leader>p", "\"_dP")    --paste w/o copying
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- bloque abajo
